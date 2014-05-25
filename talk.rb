cf743f86ffd98eb1802156f34ce1c812a720b857
中川 慎哉 <negi.magnet@gmail.com>
2014-05-26 00:54:24 +0900

    2次マルコフで実装

--- talk.rb
+++ talk.rb
@@ -1,1 +1,86 @@
-print "World!"
+#usr/bin/ruby
+require './twitter_config'
+
+# 辞書から文章生成、ツイート
+# 2次マルコフ連鎖を想定
+
+lines = 0			# 行数
+map = Hash.new(0)
+text = ""			# 生成文字列
+w1 = w2 = w3 = ""		# 直前の単語
+
+# 文章生成
+open("dic.txt") { |file|
+	inputs = file.readlines
+	inputs.each do |l|
+		lines += 1
+		words = l.split(',')
+		map[words[0..2]] = words[3].to_i
+	end
+
+	# 最初の1語を決定
+	index = rand(lines)
+	firstWords = inputs[index].split(',')
+	w1 = firstWords[0]
+	w2 = firstWords[1]
+	w3 = firstWords[2]
+	text += w1 + w2 + w3
+}
+
+lim = 140 - w1.length - w2.length - w3.length
+while lim<0 do
+	# 2分探索でmapからw2,w3をw1,w2として持つ要素群の先頭を探す
+	l = 0
+	r = lines-1
+	found = false
+	while l < r do
+		center = (l+r) / 2
+		if map[center][0..1] < [w2,w3] then
+			l = center+1
+		elsif map[center][0..1] > [w2,w3] then
+			r = center-1
+		else
+			l = r = center
+			while 0<=l && map[center][0..1] == map[l][0..1] do
+				l -= 1
+			end
+			l += 1
+			while r<lines && map[center][0..1] == map[r][0..1] do
+				r += 1
+			end
+			found = true
+			break
+		end
+	end
+
+	# 見つからなかったら終了
+	if !found then
+		break
+	end
+
+	# l..r-1のカウント値の総和を計算
+	sum = 0
+	for i in l..r-1 do
+		sum += map[i][3]
+	end
+
+	# 繋ぐ単語を決定
+	rnd = rand(sum)
+	index = l
+	while index < r && 0 < rnd do
+		rnd -= map[index][3]
+		index += 1
+	end
+
+	# 文章生成とか
+	text += map[index][2]
+	lim -= map[index][2].length
+	w1 = map[index][0]
+	w2 = map[index][1]
+	w3 = map[index][2]
+
+end
+
+# twitterにツイート
+puts text
+# $client.update(text)
--- update.rb
+++ update.rb
@@ -1,1 +1,80 @@
-print "Hello!" 
+#usr/bin/ruby
+require './twitter_config'
+require 'MeCab'
+
+# 2次マルコフ連鎖のための辞書作成
+
+# ツイートを取得するユーザ達
+users = [
+	"Miku_TypeTda",
+	"Miku_TypeLat",
+	"Miku_Hatsun_bot",
+	"px_mikubot",
+	"Miku_bot_ts",
+	"miku_cho_bot",
+	"Miku_bot_Aqua",
+	"santa_miku_bot",
+	"hatune_Miku_bot",
+	"miku_miku_bot"
+];
+
+mecab = MeCab::Tagger.new('-Ochasen')
+map = Hash.new(0)
+
+# 各ユーザについて
+users.each do |user_name|
+	# ユーザのツイート取得
+	user_info = $client.user(user_name)
+	tweets = $client.user_timeline(user_info, {:count=>100})
+
+	# ツイート文のみ取得, ついでに頭の@xxxx を削除
+	tw_text = Array.new
+	tweets.each do |t|
+		txt = t.text.dup
+		start = 0
+		if txt[0] == '@' then
+			for i in 0..t.text.length do
+				if txt[i] == ' ' then
+					start = i+1
+					break
+				end
+			end
+		end
+		tw_text.push(txt[start..txt.length-1])
+	end
+	# 重複ツイートを削除
+	tw_text.uniq!
+
+	# 形態素解析
+	tw_text.each do |t|
+#		puts t
+		node = mecab.parseToNode(t)
+		pre2 = nil
+		pre = nil
+		while node != nil do
+			if pre2 == nil then
+				pre2 = node.surface
+			elsif pre == nil then
+				pre = node.surface
+			else
+#				print node.surface
+#				print '/'
+				map[[pre2, pre,node.surface]] = map[[pre2, pre,node.surface]] + 1
+				pre2 = pre
+				pre = node.surface
+			end
+			node = node.next
+		end
+#		puts ''
+	end
+end
+
+# 辞書更新
+# map.sort {|a, b| (a[0][0] != b[0][0]) ? (a[0][0] <=> b[0][0]) : ((a[0][1] != b[0][1]) ? (a[0][1] <=> b[0][1]) : (a[0][2] <=> b[0][2])) }
+f = open("dic.txt", "w")
+map.sort{|a, b| a[0] <=> b[0]}.to_a.each do |m|
+	f.puts(m[0][0] + ',' + m[0][1] + ',' + m[0][2] + ',' + m[1].to_s)
+end
+f.close
+
+puts "finished!"
